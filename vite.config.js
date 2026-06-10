942dc2b29fe44064b065e7ca8f3b9b51af9da8c2
Muhammad Ikhsan <muhammad.ikhsan@gtech.digital>
2026-06-10 14:32:21 +0700

    chore: remove GitHub Pages, use Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/cfe-dashboard/',
+  base: '/',
   plugins: [vue()],
   server: {
     port: 7890,
